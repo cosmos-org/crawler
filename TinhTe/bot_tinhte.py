fa59b4fe4308c8371486a3a05cc08e6ca7166ae0
Duongdd <vipdarkknight07@gmail.com>
2021-12-01 12:53:43 +0700

    update comment, authors tinh te

--- TinhTe/bot_tinhte.py
+++ TinhTe/bot_tinhte.py
@@ -22,6 +22,7 @@ class BotTinhte:
         client = MongoClient('mongodb+srv://duongdt:duongdt123@demo.pe29x.mongodb.net')
         mydb = client['cosmos']
         self.mycol = mydb['opinions']
+        self.mycola = mydb['authors']
         self.NUMBER_CLICK_MORE_DATA = 500
         PROXY = 'http://127.0.0.1:8118'
         self.list_url_post = []
@@ -102,13 +103,56 @@ class BotTinhte:
         post['source'] = 'Tinh Te'
         self.mycol.insert_one(post)
 
+    def crawl_comment(self, post_url):
+        random_sleep()
+        list_comment = []
+        self.driver.get(post_url)
+        ele_comment = self.driver.find_elements(By.XPATH, "//div[contains(@class, 'thread-comment__box')]")
+        for ele in ele_comment:
+            try:
+                comment = {'source': 'Tinh Te', 'type': 'comment', 'crawled_time': datetime.utcnow(), 'rating': '-1',
+                           'comment_number': '-1', 'url': post_url}
+                ele_author = ele.find_element(By.XPATH, "div[1]/div[1]/a[1]")
+                comment['author'] = ele_author.text
+                comment['url_author'] = ele_author.get_attribute('href')
+                ele_time = ele.find_element(By.XPATH, "div[1]/div[1]/a[2]/span")
+                comment['created_time'] = ele_time.text
+                ele_content = ele.find_element(By.XPATH, "div[2]/div/div/div")
+                comment['content'] = ele_content.text
+                list_comment.append(comment)
+            except:
+                pass
+        self.mycol.insert_many(list_comment)
+
+    def crawl_author(self, author_url):
+        author = {}
+        self.driver.get(author_url)
+        ele_name = self.driver.find_element(By.XPATH, "//h1[contains(@class, 'username')]")
+        ele_intro = self.driver.find_element(By.XPATH, "//div[contains(@class, 'bio')]")
+        ele_follower = self.driver.find_element(By.XPATH, "//div[contains(@class, 'intro')]/span[1]")
+        ele_following = self.driver.find_element(By.XPATH, "//div[contains(@class, 'intro')]/span[2]")
+        ele_age = self.driver.find_element(By.XPATH, "//div[contains(@class, 'user-stats')]/table/tbody/tr[2]/td[2]")
+        ele_npost = self.driver.find_element(By.XPATH, "//div[contains(@class, 'user-stats')]/table/tbody/tr[3]/td[2]")
+        ele_nlike = self.driver.find_element(By.XPATH, "//div[contains(@class, 'user-stats')]/table/tbody/tr[4]/td[2]")
+        author['url'] = author_url
+        author['name'] = ele_name.text
+        author['intro'] = ele_intro.text
+        author['follower'] = ele_follower.text
+        author['following'] = ele_following.text
+        author['age'] = ele_age.text
+        author['number_post'] = ele_npost.text
+        author['number_like'] = ele_nlike.text
+        self.mycola.insert_one(author)
+
 
 if __name__ == '__main__':
     a = BotTinhte()
-    a.parse()
-    for i in a.list_url_post:
-        try:
-            a.crawl_post_content(i)
-        except:
-            print("Loi")
+    a.crawl_author("https://tinhte.vn/profile/duy-luan.39792/")
+    # a.crawl_comment('https://tinhte.vn/thread/trai-nghiem-ban-phim-logitech-mx-key-ban-full-du-do-choi-hon.3441999')
+    # a.parse()
+    # for i in a.list_url_post:
+    #     try:
+    #         a.crawl_post_content(i)
+    #     except:
+    #         print("Loi")
     a.close()
